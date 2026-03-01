d847f2ac268779a780cba0acd9d89b991e8fee42
Tint Wai Yan Min <andrew@Tints-MacBook-Pro.local>
2026-03-01 22:44:21 +0800

    Fix UI

--- resources/js/pages/settings/profile.tsx
+++ resources/js/pages/settings/profile.tsx
@@ -1,6 +1,5 @@
 import { Transition } from '@headlessui/react';
 import { Form, Head, Link, usePage } from '@inertiajs/react';
-import ProfileController from '@/actions/App/Http/Controllers/Settings/ProfileController';
 import DeleteUser from '@/components/delete-user';
 import Heading from '@/components/heading';
 import InputError from '@/components/input-error';
@@ -13,6 +12,7 @@ import SettingsLayout from '@/layouts/settings/layout';
 import { edit } from '@/routes/profile';
 import { send } from '@/routes/verification';
 import type { BreadcrumbItem, SharedData } from '@/types';
+import ProfileController from '@/actions/App/Http/Controllers/Settings/ProfileController';
 
 export default function Profile({
     mustVerifyEmail,
